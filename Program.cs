22729b9d9f6944e542b88bd09059645982caec32
Terry D. Eppler <terry.eppler@gmail.com>
2020-12-02 19:04:02 -0600

    added ico and png files for the form decorations; rebuild;

--- Program.cs
+++ Program.cs
@@ -1,17 +1,27 @@
-﻿using System;
-
-namespace BudgetExecution
+﻿namespace BudgetExecution
 {
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// 
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Program"/> class.
+        /// </summary>
         public Program()
         {
         }
 
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
         [ STAThread ]
         public static void Main()
         {
-            System.Windows.Forms.Application.Run( new BadgerMain() );
+            Application.Run( new BadgerMain() );
         }
     }
 }
